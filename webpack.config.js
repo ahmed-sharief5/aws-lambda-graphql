117cf621d1308cd8e3d23970bc225d37f24eef88
Ahmed Sharief <shariefeng05@gmail.com>
2022-03-04 14:46:22 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const { join } = require(`path`)
+// https://github.com/serverless-heaven/serverless-webpack
+const slsw = require(`serverless-webpack`)
+// https://github.com/liady/webpack-node-externals
+const nodeExternals = require(`webpack-node-externals`)
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? `development` : `production`,
+  entry: slsw.lib.entries,
+  target: `node`,
+  externals: [nodeExternals()],
+  devtool: `nosources-source-map`,
+  output: {
+    libraryTarget: `commonjs`,
+    path: join(__dirname, `.webpack`),
+    filename: `[name].js`,
+    sourceMapFilename: `[file].map`
+  },
+  stats: {
+    colors: true,
+    reasons: false,
+    chunks: false
+  },
+  performance: {
+    hints: false
+  },
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: `babel-loader`,
+        exclude: /node_modules/,
+        options: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: `graphql-tag/loader`
+      }
+    ]
+  }
+}
